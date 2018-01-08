54f889660832548a2fb344d7b2beb57d2f8b4515
rkdyava <30162035+rkdyava@users.noreply.github.com>
2018-01-08 23:23:02 +0100

    Add files via upload
    my basic coding fro factors

---
+++ hello.cpp
@@ -0,0 +1,26 @@
+
+// my first program in C++
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    int number, i;
+
+    cout << "Enter an integer: "<<endl;
+    cin >> number;
+
+    cout << "Factors of" << number<<" is:"<<endl;
+for(i=1;i<=number;i++)
+{
+if(number%i==0)
+cout<< i <<endl;
+else
+cout<<"---"<<endl;
+
+
+}    
+    return 0;
+}
+
+
